46e9b4cfe9ba2a1c8568ab0cae0bd962ba79df7c
dayang <1782055745@qq.com>
2022-10-20 17:41:24 +0800

    todolist列表的开发

---
+++ vue-advance/src/components/MyHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="todo-header">
+      <input
+        type="text"
+        v-model="title"
+        @keydown.enter="add"
+        placeholder="请输入你的任务名称，按回车键确认"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { nanoid } from "nanoid";
+//引入组件
+export default {
+  name: "MyHeader",
+  props: ["todos", "addtodo"],
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    add() {
+      if (!this.title.trim) return alert("请输入一个数据");
+      const todoObject = { id: nanoid(), title: this.title, isDone: false };
+      this.addtodo(todoObject);
+      this.title = "";
+    },
+  },
+};
+</script>
+<style scoped>
+/*header*/
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
+    0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
